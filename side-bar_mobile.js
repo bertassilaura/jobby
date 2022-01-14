13e19f616c191717e612c1027bf730d5f8b27131
g-buzzi <86495262+g-buzzi@users.noreply.github.com>
2022-01-14 00:45:09 -0300

    CSS padronizado

--- side-bar_mobile.js
+++ side-bar_mobile.js
@@ -1,9 +1,9 @@
 function open_side_bar(){
     document.getElementById("mobile-side-bar").style.height = "100%";
-    document.getElementById("mobile-header").getElementsByTagName("a")[0].style.opacity = "0";
+    document.getElementById("open-side-bar").style.opacity = "0";
 };
 
 function close_side_bar(){
     document.getElementById("mobile-side-bar").style.height = "0px";
-    document.getElementById("mobile-header").getElementsByTagName("a")[0].style.opacity = "1";
+    document.getElementById("open-side-bar").style.opacity = "1";
 };
